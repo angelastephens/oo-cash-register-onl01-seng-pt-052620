e807b57278b655becbce38ee9dfcd00cd119c3f1
Angela Stephens <angelastephens317@gmail.com>
2020-06-20 02:48:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,10 +9,7 @@ class CashRegister
   
   def add_item(title, price, quantity=1)
     self.total += price * quantity
-    quantity.times do
-      items << title
-    end
-    self.last_transaction = amount * quantity
+    
   end
   
   def apply_discount(discount=0)
